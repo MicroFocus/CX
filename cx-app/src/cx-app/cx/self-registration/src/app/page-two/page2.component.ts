fa1319a64477c0e61b8ed01bd0f734542543eefc
Zack Grossbart <Zack.Grossbart@microfocus.com>
2017-03-24 13:11:23 -0400

    Added the controls for page 2 and wired them up to the controller.

--- cx-app/src/cx-app/cx/self-registration/src/app/page-two/page2.component.ts
+++ cx-app/src/cx-app/cx/self-registration/src/app/page-two/page2.component.ts
@@ -6,4 +6,47 @@ import {Component} from 'ng-metadata/core';
     template: require('./page2.component.html')
 })
 export class PageTwoComponent {
+    public m_name;
+    public m_famName;
+    public m_title;
+    public m_email;
+    public m_bDay;
+    public m_bMonth;
+    public m_bYear;
+    public m_zip;
+    public m_birth;
+    
+    public m_officeOptions = [];
+    public m_office;
+
+    constructor() {
+        this.m_officeOptions.push({
+            name: 'Bangalore',
+            id: 'bangalore'
+        });
+
+        this.m_officeOptions.push({
+            name: 'Cambridge',
+            id: 'cambridge'
+        });
+
+        this.m_officeOptions.push({
+            name: 'Houston',
+            id: 'houston'
+        });
+
+        this.m_officeOptions.push({
+            name: 'Provo',
+            id: 'provo'
+        });
+
+        this.m_office = this.m_officeOptions[1];
+    }
+
+    public showNext(): void {
+        console.log('m_name: ' + this.m_name);
+        this.m_birth = new Date(parseInt(this.m_bYear, 10), parseInt(this.m_bMonth, 10) - 1, parseInt(this.m_bDay, 10));
+
+        console.log('m_birth: ' + this.m_birth);
+    }
 }
