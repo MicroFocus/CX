4388e0bcd12ccb083c44ad2854f386891960e5fd
James Albright <James.Albright@microfocus.com>
2017-04-11 11:11:45 -0600

    Implemented the password strength meter, as given in Lynn's mockup.

--- cx-app/src/cx-app/cx/self-registration/src/app/page-three/page3.component.ts
+++ cx-app/src/cx-app/cx/self-registration/src/app/page-three/page3.component.ts
@@ -6,7 +6,41 @@ import { Component } from 'ng-metadata/core';
     template: require( './page3.component.html' )
 } )
 export class PageThreeComponent {
-    public Reg(): void {
-        console.log( 'hello from page 3' );
+    private password: string;
+    private pwStrengthText: string;
+    private pwStrengthClass: string;
+
+    onPasswordChange(): void {
+        switch (this.password.length) {
+            case 0:
+                this.pwStrengthClass = '';
+                this.pwStrengthText = '';
+                break;
+
+            case 1:
+                this.pwStrengthClass = 'mf-icon-strength1';
+                this.pwStrengthText = 'Dangerous';
+                break;
+
+            case 2:
+                this.pwStrengthClass = 'mf-icon-strength2';
+                this.pwStrengthText = 'Weak';
+                break;
+
+            case 3:
+                this.pwStrengthClass = 'mf-icon-strength3';
+                this.pwStrengthText = 'Normal';
+                break;
+
+            case 4:
+                this.pwStrengthClass = 'mf-icon-strength4';
+                this.pwStrengthText = 'Strong';
+                break;
+
+            default:
+                this.pwStrengthClass = 'mf-icon-strength5';
+                this.pwStrengthText = 'Stronger';
+                break;
+        }
     }
 }
