c30ed6206e5b91738c1f1b1575c86489284e42d5
Zack Grossbart <Zack.Grossbart@microfocus.com>
2017-06-12 10:53:39 -0400

    We are now getting the list of totals and sending them back to the dashboard component.

--- ig-overview/src/components/dashboard/dashboard.component.ts
+++ ig-overview/src/components/dashboard/dashboard.component.ts
@@ -1,18 +1,37 @@
 import { Component } from '../../component.decorator';
+import { UserData, Totals } from '../../services/gromit-service';
 
 @Component({
     templateUrl: require('./dashboard.component.html')
 })
 export default class DashboardComponent {
     private clientid: string = 'iac2';
-    private arserver: string = 'http://jalbright:8080';
-    private ospserver: string = 'http://jalbright:8080';
+    private arserver: string = 'http://192.168.0.76:8080';
+    private ospserver: string = 'http://192.168.0.76:8080';
+
+    private userName: string;
+    private totals: Totals;
 
     static $inject = ['gromitService'];
     constructor(private gromitService) {
     }
 
     getSomeData() {
-        this.gromitService.fetchUsers(this.clientid, this.arserver, this.ospserver, 'bjones');
+        let gromitService = this.gromitService;
+        let dc: DashboardComponent = this;
+
+        gromitService.whoAmI(this.clientid, this.arserver, this.ospserver, function(data: UserData) {
+            dc.userName = data.userName;
+
+            gromitService.getTotals(function(totals: Totals) {
+                dc.totals = totals;
+            });
+
+            gromitService.fetchUsers(function(users: any) {
+                console.log('number of users', users.arraySize);
+
+            });
+
+        });
     }
 }
--- ig-overview/src/services/gromit-service.ts
+++ ig-overview/src/services/gromit-service.ts
@@ -1,7 +1,35 @@
 declare const gromit: any;
 
+export class UserData {
+    userName: string;
+    userId: Number;
+
+    constructor(userName: string, userId: Number) {
+        this.userName = userName;
+        this.userId = userId;
+
+    }
+}
+
+export class Totals {
+    private totals: Object;
+
+    constructor(totals: any) {
+        this.totals = {};
+        for (let total of totals.entities) {
+            this.totals[total.entityType] = total.entityCount;
+        }
+    }
+
+    getTotal(type: string): Number {
+        return Number(this.totals[type]);
+    }
+}
+
 export default class GromitService {
     static $inject = ['$http', '$rootScope', '$window'];
+    private apiserver: string;
+
     constructor(private $http) {
     }
 
@@ -13,18 +41,42 @@ export default class GromitService {
         // gromit.addCSSLink('css/gromitsample.css');
     }
 
-    fetchUsers(clientid, authserver, apiserver) {
+    whoAmI(clientid, authserver, apiserver, callback) {
+        let http = this.$http;
         gromit.ClientId = clientid;
+        this.apiserver = apiserver;
 
         gromit.AuthUrl = authserver + '/osp/a/idm/auth/oauth2/grant';
         gromit.AuthLogoutUrl = authserver + '/osp/a/idm/auth/app/logout';
-        gromit.get(apiserver + '/api/whoami', this.$http, function(data) {
-            const url = apiserver + '/api/data/users/search/?sortBy=displayName&sortOrder=ASC&indexFrom=0&size=50&showCt=true&listAttr=displayName&listAttr=jobTitle&qMatch=ANY';
+        gromit.get(apiserver + '/api/whoami', http, function(data) {
+            this.user = data.principal;
+            callback(new UserData(data.principal, data.userId));
+        });
+    }
 
-            gromit.post(url, this.$http, '{}', function(data) {
-                console.log('Fetched gromit data');
-                console.log('data: %O', data);
-            });
+    fetchUsers(callback) {
+        if (gromit.ClientId == null) {
+            throw new Error('You must call who am I before making other calls.');
+        }
+        let http = this.$http;
+
+        let url = this.apiserver + '/api/data/users/search/?sortBy=displayName&sortOrder=ASC&indexFrom=0&size=50&showCt=true&listAttr=displayName&listAttr=jobTitle&qMatch=ANY';
+
+        gromit.post(url, http, '{}', function(data) {
+            callback(data);
+        });
+    }
+
+    getTotals(callback) {
+        if (gromit.ClientId == null) {
+            throw new Error('You must call who am I before making other calls.');
+        }
+        let http = this.$http;
+
+        let url = this.apiserver + '/api/data/totals/';
+
+        gromit.get(url, http, function(data) {
+            callback(new Totals(data));
         });
     }
 }
