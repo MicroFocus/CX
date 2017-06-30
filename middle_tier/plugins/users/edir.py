822a03a1c1c88764199bee63debc5218f8b519dd
Zack Grossbart <Zack.Grossbart@microfocus.com>
2017-06-30 10:51:53 -0400

    We are now able to create a user in eDirectory again.

--- middle_tier/plugins/users/edir.py
+++ middle_tier/plugins/users/edir.py
@@ -17,22 +17,26 @@ class EDirUsersResource(Resource):
         log.info("Create user body:{}".format(request.data))
         data = json.loads(request.data)
         username = data["name"]
+        print('username: ' + username)
+        
         server = Server('10.120.46.80')
         conn = Connection(server, "cn=zgrossbart,ou=services,o=corp", "N0v3ll123")
         conn.bind()
         attrs = {}
         attrs['cn'] = 'zacktest'
-        attrs['sn'] = 'App'
-        attrs['userPassword'] = 'aDifferentSecret'
+        attrs['sn'] = 'Zack\'s test user'
+#        attrs['userPassword'] = 'aDifferentSecret'
         attrs['fullName'] = 'A test user from Zack'
         attrs['givenName'] = 'Zack'
-        attrs['Language'] = 'ENGLISH'
+#        attrs['Language'] = 'ENGLISH'
         attrs['description'] = 'This is a user Zack created from Python'
-        attrs['passwordAllowChange'] = 'TRUE'
-        dn = "cn=zacktest221,ou=SA,ou=CanadaLife,o=corp"
+#        attrs['passwordAllowChange'] = 'TRUE'
+        dn = "cn=zacktest,ou=SA,ou=CanadaLife,o=corp"
         result = conn.add(dn, ','.join(['inetOrgPerson']), attrs)
+        
         if result:
             return Response(json.dumps({"result": True}), headers={'Content-type': 'application/json'})
+            
         log.error('Failed to add user: {}'.format(conn.result))
         return Response(json.dumps({"result": False, "error": conn.result}),
                         headers={'Content-type': 'application/json'}, status_code=500)
