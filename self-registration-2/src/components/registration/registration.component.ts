0f8848cc53f40ceb49176e047b3fb91d5fdc3600
James Albright <James.Albright@microfocus.com>
2017-07-07 14:10:45 -0600

    Added basic validation to many of the fields on the form.

--- self-registration-2/src/components/registration/registration.component.ts
+++ self-registration-2/src/components/registration/registration.component.ts
@@ -7,12 +7,17 @@ import { User } from '../../services/user.service';
 })
 export default class RegistrationComponent {
     private validationErrors: string[];
+    private fieldErrors: any;
 
     private m_fName: string;
     private m_sName: string;
+    private m_policyNumber: string;
     private m_password: string;
+    private m_confirmPassword: string;
     private m_email: string;
+    private m_confirmEmail: string;
     private m_postalCode: string;
+    private m_termsConsent: boolean;
 
     static $inject = ['$state', 'userService'];
     constructor(private $state: angular.ui.IStateService, private userService: UserService) {
@@ -20,11 +25,7 @@ export default class RegistrationComponent {
     }
 
     public createUser(): void {
-        this.validateField(this.m_fName, 'First name is required.');
-        this.validateField(this.m_sName, 'Surname is required.');
-        this.validateField(this.m_password, 'Password is required.');
-        this.validateField(this.m_email, 'Email is required.');
-        this.validateField(this.m_postalCode, 'Post code is required.');
+        this.validateFields();
 
         if (this.validationErrors.length == 0) {
             const user: User  = new User();
@@ -46,8 +47,43 @@ export default class RegistrationComponent {
         }
     }
 
-    private validateField(field: string, errorMsg: string) {
-        if (!field) {
+    private validateFields() {
+        this.fieldErrors = {};
+        this.validationErrors = [];
+
+        this.validateFieldHasText('firstName', this.m_fName, 'First name is required.');
+        this.validateFieldHasText('surName', this.m_sName, 'Surname is required.');
+        this.validateFieldHasText('policyNumber', this.m_policyNumber, 'Policy number is required.');
+        this.validateFieldHasText('email', this.m_email, 'Email is required.');
+        this.validateFieldHasText('confirmEmail', this.m_confirmEmail, 'Confirmation email is required.');
+        this.validateFieldHasText('password', this.m_password, 'Password is required.');
+        this.validateFieldHasText('confirmPassword', this.m_confirmPassword, 'Confirmation password is required.');
+        this.validateTrue('termsConsent', this.m_termsConsent, 'You must agree to the terms and conditions before' +
+                ' you can register.');
+
+        if (this.m_email && this.m_confirmEmail && this.m_email !== this.m_confirmEmail) {
+            let errMsg = 'The confirmation email address does not match the email address.';
+            this.fieldErrors['confirmEmail'] = errMsg;
+            this.validationErrors.push(errMsg);
+        }
+
+        if (this.m_password && this.m_confirmPassword && this.m_password !== this.m_confirmPassword) {
+            let errMsg = 'The password and confirmation password do not match.';
+            this.fieldErrors['confirmPassword'] = errMsg;
+            this.validationErrors.push(errMsg);
+        }
+    }
+
+    private validateFieldHasText(fieldName: string, fieldValue: string, errorMsg: string) {
+        if (!fieldValue) {
+            this.fieldErrors[fieldName] = errorMsg;
+            this.validationErrors.push(errorMsg);
+        }
+    }
+
+    private validateTrue(fieldName: string, fieldValue: boolean, errorMsg: string) {
+        if (!fieldValue) {
+            this.fieldErrors[fieldName] = errorMsg;
             this.validationErrors.push(errorMsg);
         }
     }
