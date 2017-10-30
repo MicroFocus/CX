9d21c169196bcf72395211514f8bede09fa33f32
Zack Grossbart <Zack.Grossbart@microfocus.com>
2017-10-30 11:54:44 -0400

    We are now making sure that we don't use the Gromit CSS.

--- token-validation/src/services/gromit-service.ts
+++ token-validation/src/services/gromit-service.ts
@@ -10,6 +10,12 @@ export default class GromitService {
     }
 
     init() {
+        gromit.addCSSLink = function() {
+            /*
+             * We are doing this so we don't get the Gromit CSS.  Gromit has added
+             * a real flag for this in the latest release, but it isn't out yet.
+             */
+        };
         gromit.init();
     }
 
