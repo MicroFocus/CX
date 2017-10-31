69f80dc97fa885f2e6701ad4bd779f0744d51607
Zack Grossbart <Zack.Grossbart@microfocus.com>
2017-10-31 09:53:59 -0400

    Added a bunch of documentation comments to the custom.py file.

--- middle_tier/security/custom.py
+++ middle_tier/security/custom.py
@@ -18,6 +18,32 @@ class CustomKeyHandler(SecurityHandler):
 
 
 class CustomKeySecurityHandlerWrapper(SecurityHandler):
+    """
+    This class handles the caching of the security token.  It determines when you should 
+    get the security token from cache and when we should call to OSP to validate the token.
+    
+    Right now we are using this class for OSP tokens, but it is just a generic cache.  It could
+    support any type of token or other data we need at some point in the future.
+    
+    This class uses a REDIS cache so we can have a shared cache instead of relying on Python 
+    threads.  Our application uses uWSGI which separates HTTP calls on processes instead of 
+    threads.  That means we don't share memory between the threads.  If we try to we will
+    run into trouble with the Python Global Interpreter Lock.
+    
+    Instead of that we are running Redis in a separate process to manage the cache of tokens.
+    By using this separate process we can avoid validating the OSP token on every request and
+    gain a substantial performance improvement.
+    
+    Right now we are using redis, but this class is a generic cache interface.  It could work 
+    with memcached or any other cache we wanted to use.  There is more documentation about that
+    here:
+    
+        http://werkzeug.pocoo.org/docs/0.12/contrib/cache
+    
+    This would all be avoidable if we were using JWT tokens.  With JWT we could validate the
+    token without making a call to OSP and we wouldn't have as much need for caching, but JWT
+    isn't available on the versions of OSP we need to support.
+    """
     cache = None
 
     def __init__(self, auth_data):
