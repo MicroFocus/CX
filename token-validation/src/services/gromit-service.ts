79d52d34044441e339029999a8facd3d92b2328d
Zack Grossbart <Zack.Grossbart@microfocus.com>
2017-11-10 06:51:55 -0500

    We are now doing showing errors in a nicely formatted error box in the UI instead of just showing them in the browser console.

--- token-validation/src/services/gromit-service.ts
+++ token-validation/src/services/gromit-service.ts
@@ -10,7 +10,15 @@ export default class GromitService {
     }
 
     init() {
+        /*
+         * By default Gromit includes some general CSS files like reset.css.  We 
+         * aren't loading those in this project so we set the addCSS property in 
+         * Gromit to false.  However, we still want to show Gromit style error 
+         * messages so we are loading the humanMsg.css and we've added styling for
+         * the Gromit fatal error message panel to our main.scss.
+         */
         gromit.addCSS = false;
+        gromit.addCSSLink('gromit/css/humanMsg.css');
         gromit.init();
     }
 
@@ -38,6 +46,17 @@ export default class GromitService {
 
         gromit.get('/api/osp/token', http, function(data) {
             callback(data);
+        }, function(code, subcode, reason) {
+            /*
+             * This function gets called if we have an error in the special Micro Focus error
+             * format.  This error format is returned from most of the Micro Focus REST endpoints
+             * and gives us extra information like error codes and subcodes so we can do more
+             * specific error handling.
+             */
+            gromit.showGeneralError(code, subcode, reason);
+        }, function(data) {
+            // This function gets called for general errors.
+            gromit.showFatalError(data.error);
         });
     }
 }
