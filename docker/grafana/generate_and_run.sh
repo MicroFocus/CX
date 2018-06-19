0f5db26f9edaefd54da9c76678b29bae2129f2d8
alpost <p_alber@yahoo.com>
2018-06-19 15:01:02 -0400

    IG dashboard work in progress

--- docker/grafana/generate_and_run.sh
+++ docker/grafana/generate_and_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 jsonnet -J /tmp/grafonnet-lib  /tmp/dashboard.jsonnet -o /data/dashboards/dashboard.json
-jsonnet /tmp/ig-dashboard.jsonnet -V IGHOST -o /data/dashboards/ig-dashboard.json
+jsonnet /tmp/ig-dashboard.jsonnet -V IGHOST -V GHOST -o /data/dashboards/ig-dashboard.json
+
 jsonnet /tmp/datasource.jsonnet -V IGARA_DB_URL -V IGARA_DB_PASSWORD -V IGARA_DB_USER -V IGARA_DB_NAME \
 -V IGOPS_DB_URL -V IGOPS_DB_PASSWORD -V IGOPS_DB_USER -V IGOPS_DB_NAME -o /etc/grafana/provisioning/datasources/datasource.yml
 /run.sh
