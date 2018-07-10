d68919549686b2a6bb3f2d2a1ec2113b5bba40c1
Denis Nasyrov <nasyrovd@gmail.com>
2018-07-10 16:05:10 +0500

    Folders, init scripts for postgres, projects dashboards

--- docker/grafana/generate_and_run.sh
+++ docker/grafana/generate_and_run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 jsonnet -J /tmp/grafonnet-lib  /tmp/dashboard.jsonnet -o /data/dashboards/dashboard.json
-jsonnet /tmp/ig-dashboard.jsonnet -V IGHOST -V GHOST -o /data/dashboards/ig-dashboard.json
+
+# generating multiple dashbords
+# maybe we should automate this action?
+mkdir -p /data/dashboards/ig
+jsonnet /tmp/provisioning/ig/ig-dashboard.jsonnet -V IGHOST -V GHOST -o /data/dashboards/ig/ig-dashboard.json
+
+mkdir -p /data/dashboards/aaf
+jsonnet /tmp/provisioning/aaf/aaf-dashboard.jsonnet -V IGHOST -V GHOST -o /data/dashboards/aaf/aaf-dashboard.json
 
 jsonnet /tmp/datasource.jsonnet -V IGARA_DB_URL -V IGARA_DB_PASSWORD -V IGARA_DB_USER -V IGARA_DB_NAME \
 -V IGOPS_DB_URL -V IGOPS_DB_PASSWORD -V IGOPS_DB_USER -V IGOPS_DB_NAME -o /etc/grafana/provisioning/datasources/datasource.yml
